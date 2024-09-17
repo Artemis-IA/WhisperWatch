f67e88df1cfa4cb5ab8f640af8ed0e3c3a6ea52c
ibrahim.m <ibrahim.mohammad@arkea.com>
2024-09-17 14:11:17 +0200

    major fixes

--- app/services/download_service.py
+++ app/services/download_service.py
@@ -3,16 +3,14 @@ import yt_dlp
 import os
 import re
 import logging
-from apscheduler.schedulers.background import BackgroundScheduler
 
 class DownloadService:
     YOUTUBE_URL_REGEX = re.compile(r'^(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/.+$')
 
-    def __init__(self, download_path='downloads', kafka_topic='new_videos'):
+    def __init__(self, download_path='downloads'):
         self.download_path = download_path
         if not os.path.exists(download_path):
             os.makedirs(download_path)
-
 
     def download_audio(self, youtube_url):
         """Download only the audio from a YouTube video."""
@@ -26,7 +24,6 @@ class DownloadService:
             }],
             'quiet': True,
             'PROXY': 'http://localhost:3128'
-
         }
 
         try:
@@ -36,6 +33,24 @@ class DownloadService:
                 audio_file = os.path.splitext(audio_file)[0] + '.mp3'
                 return audio_file
         except yt_dlp.utils.DownloadError as e:
+            logging.error(f"Failed to download audio: {e}")
+            return None
+
+    def download_video(self, youtube_url):
+        """Download the full video from YouTube."""
+        ydl_opts = {
+            'format': 'bestvideo+bestaudio',
+            'outtmpl': os.path.join(self.download_path, '%(id)s.%(ext)s'),
+            'quiet': True,
+            'PROXY': 'http://localhost:3128'
+        }
+
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(youtube_url, download=True)
+                video_file = ydl.prepare_filename(info_dict)
+                return video_file
+        except yt_dlp.utils.DownloadError as e:
             logging.error(f"Failed to download video: {e}")
             return None
 
@@ -43,6 +58,10 @@ class DownloadService:
         """Download videos based on URL input."""
         logging.info(f"Downloading video: {video_url}")
         if self.YOUTUBE_URL_REGEX.match(video_url):
-            self.download_video(video_url)
+            video_file = self.download_video(video_url)
+            if video_file:
+                logging.info(f"Video downloaded successfully: {video_file}")
+            else:
+                logging.error(f"Failed to download video from URL: {video_url}")
         else:
             logging.error(f"Invalid video URL: {video_url}")
